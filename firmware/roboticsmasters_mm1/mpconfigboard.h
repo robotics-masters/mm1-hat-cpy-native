b269c6060685db5b0683c260b35db2b8198bbcb0
wallarug <wallarug@github.com>
2019-02-05 21:49:48 +1100

    fixed up config files to use external flash.

--- firmware/roboticsmasters_mm1/mpconfigboard.h
+++ firmware/roboticsmasters_mm1/mpconfigboard.h
@@ -1,24 +1,20 @@
-#define MICROPY_HW_BOARD_NAME "Robotics Masters MM1"
+#define MICROPY_HW_BOARD_NAME "Robotics Masters MM1 HAT"
 #define MICROPY_HW_MCU_NAME "samd21g18"
 
-#define MICROPY_HW_LED_STATUS   (&pin_PA12)
+#define MICROPY_HW_LED_STATUS   (&pin_PA17)
 
-// No framebuf on CRICKit version to save space.
-#define MICROPY_PY_FRAMEBUF         (0)
+//#define MICROPY_HW_NEOPIXEL (&pin_PA06)
 
-// Salae reads 12mhz which is the limit even though we set it to the safer 8mhz.
-#define SPI_FLASH_BAUDRATE  (8000000)
-
-// On-board flash
-#define SPI_FLASH_MOSI_PIN          &pin_PA20
-#define SPI_FLASH_MISO_PIN          &pin_PA16
-#define SPI_FLASH_SCK_PIN           &pin_PA21
-#define SPI_FLASH_CS_PIN            &pin_PB22
+#define SPI_FLASH_MOSI_PIN          &pin_PA08
+#define SPI_FLASH_MISO_PIN          &pin_PA14
+#define SPI_FLASH_SCK_PIN           &pin_PA09
+#define SPI_FLASH_CS_PIN            &pin_PA13
 
 // These are pins not to reset.
-#define MICROPY_PORT_A        (0)
-#define MICROPY_PORT_B        (0)
-#define MICROPY_PORT_C        (0)
+#define MICROPY_PORT_A        ( 0 ) //PORT_PA06
+#define MICROPY_PORT_B        ( 0 )
+#define MICROPY_PORT_C        ( 0 )
+
 
 // If you change this, then make sure to update the linker scripts as well to
 // make sure you don't overwrite code.
@@ -26,10 +22,8 @@
 
 #define BOARD_FLASH_SIZE (0x00040000 - 0x2000 - CIRCUITPY_INTERNAL_NVM_SIZE)
 
+//#define BOARD_HAS_CRYSTAL 1
 #define CALIBRATE_CRYSTALLESS 1
-
-// Explanation of how a user got into safe mode.
-#define BOARD_USER_SAFE_MODE_ACTION "pressing both buttons at start up"
 
 #define DEFAULT_I2C_BUS_SCL (&pin_PA22)
 #define DEFAULT_I2C_BUS_SDA (&pin_PA23)
@@ -45,4 +39,4 @@
 #define IGNORE_PIN_PA24     1
 #define IGNORE_PIN_PA25     1
 
-#define CIRCUITPY_DISPLAYIO (0)
+#define CIRCUITPY_I2CSLAVE
