593789bf760052316b12a24e88bbf44709652880
wallarug <wallarug@github.com>
2019-02-11 21:08:12 +1100

    working with internal flash

--- firmware/roboticsmasters_mm1/mpconfigboard.mk
+++ firmware/roboticsmasters_mm1/mpconfigboard.mk
@@ -4,19 +4,24 @@ USB_PID = 0x0005
 USB_PRODUCT = "MM1 HAT"
 USB_MANUFACTURER = "RoboticsMasters"
 
-SPI_FLASH_FILESYSTEM = 1
-EXTERNAL_FLASH_DEVICE_COUNT = 1
-EXTERNAL_FLASH_DEVICES = "ST26VF016B"
-LONGINT_IMPL = MPZ
+# Non-Flash Edition
+INTERNAL_FLASH_FILESYSTEM = 1
+LONGINT_IMPL = NONE
+
+# SPI-Flash Edition
+#SPI_FLASH_FILESYSTEM = 1
+#EXTERNAL_FLASH_DEVICE_COUNT = 1
+#EXTERNAL_FLASH_DEVICES = "ST26VF016B"
+#LONGINT_IMPL = MPZ
 
 CHIP_VARIANT = SAMD21G18A
 CHIP_FAMILY = samd21
 
-CFLAGS_INLINE_LIMIT = 55
+#CFLAGS_INLINE_LIMIT = 55
 
 # Include these Python libraries in firmware.
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Crickit
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Motor
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_seesaw
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Crickit
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Motor
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_seesaw
