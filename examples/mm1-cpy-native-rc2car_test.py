579d6062e4bc4311a38fa96367ca7eec1c0cc271
wallarug <wallarug@bitbucket.org>
2019-02-19 22:29:06 +1100

    Last update from me tonight: Steering from the MM1 HAT is straight forward and done.  It uses a Servo object in CircuitPython. Throttle is not done.  It is not working correctly.  It cannot use the servo library because it requires a precise timed signal / pulse.  I have experimented with the PulseOut library but it is not reliable - the car does not read the signals correctly and ends up shunting along.

--- examples/mm1-cpy-native-rc2car_test.py
+++ examples/mm1-cpy-native-rc2car_test.py
@@ -9,8 +9,9 @@ import board
 from analogio import AnalogIn
 from digitalio import DigitalInOut, Direction
 
-from pulseio import PWMOut, PulseIn
+from pulseio import PWMOut, PulseIn, PulseOut
 from adafruit_motor import servo
+from array import array
 
 # set up on-board LED
 led = DigitalInOut(board.LED)
@@ -22,7 +23,9 @@ led.direction = Direction.OUTPUT
 pwm1 = PWMOut(board.SERVO1, duty_cycle=2 ** 15, frequency=50)
 pwm2 = PWMOut(board.SERVO2, duty_cycle=2 ** 15, frequency=50)
 steering_servo = servo.Servo(pwm1)
-throttle_servo = servo.Servo(pwm2)
+#throttle_servo = servo.Servo(pwm2)
+#steering_servo = PulseOut(pwm1)
+throttle_servo = PulseOut(pwm2)
 
 steering_channel = PulseIn(board.RCH1)
 throttle_channel = PulseIn(board.RCH2)
@@ -46,21 +49,58 @@ def get_angle(time):
 
 ## go-loop
 
+#ch1 = array('H', [1500])
+ch2 = array('H', [1500])
+
 while True:
     # get radio channel values
     #steering_servo.angle = get_angle(steering_channel)
     #throttle_servo.angle = get_angle(throttle_channel)
-    while len(steering_channel) == 0:
+    
+    ### working between here and next comment
+    while len(throttle_channel) == 0:
         pass
     
-    steering_channel.pause()
+    throttle_channel.pause()
     
-    print(get_angle(steering_channel[0]))
-    steering_servo.angle = get_angle(steering_channel[0])
+    print(throttle_channel[0])
     
-    steering_channel.clear()
+    ch2[0] = throttle_channel[0]
+    throttle_servo.send(ch2)
+    #throttle_servo.angle = get_angle(steering_channel[0])
     
-    steering_channel.resume(20000)
+    
+    throttle_channel.clear()
+    
+    throttle_channel.resume(20000)
+    
+    #time.sleep(0.01)
+    ### working between here and last comment
+    
+    # while len(steering_channel) == 0 or len(throttle_channel) == 0:
+#         if len(steering_channel) > 0: break
+#         elif len(throttle_channel) > 0: break
+#         else: pass
+    
+    ### steering
+    #if len(steering_channel) > 0:
+    #    steering_channel.pause()
+    #    ##ch1[0] = steering_channel[0]
+    #    print("Steering ",  steering_channel[0])
+    #    ##steering_servo.angle = get_angle(steering_channel[0])
+    #    steering_channel.clear()
+    #    steering_channel.resume(20000)
+    
+    ### throttle
+    #if len(throttle_channel) > 0:
+    #    throttle_channel.pause()
+    #    ##ch2[0] = throttle_channel[0]
+    #    print("Throttle ",  throttle_channel[0])
+    #    ##throttle_servo.angle = get_angle(throttle_channel[0])
+    #    throttle_channel.clear()
+    #    throttle_channel.resume(20000)
+
+    #time.sleep(0.01)
 ##    led.value = True
 ##    time.sleep(0.5)
 ##    for angle in range(0, 180, 5):
