b4531120e441d9ca859e4ffd2de0cb96bef5e4ce
wallarug <wallarug@coliemore.com.au>
2019-03-02 23:01:47 +1100

    added hello world test

---
+++ examples/mm1-cpy-native-hello-world.py
@@ -0,0 +1,14 @@
+# Write your code here :-)
+import time
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.MISO)
+led.direction = digitalio.Direction.OUTPUT
+
+while(True):
+    led.value = True
+    time.sleep(0.5)
+    led.value = False
+    print("Hello, World! This is CircuitPython")
+    time.sleep(0.5)
