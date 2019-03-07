a7729eba11b77b6357851067721b897adb57d0d7
wallarug <wallarug@coliemore.com.au>
2019-03-07 23:34:28 +1100

    updated flash version script

--- firmware/mm1_hat_flash/mpconfigboard.mk
+++ firmware/mm1_hat_flash/mpconfigboard.mk
@@ -5,8 +5,8 @@ USB_PRODUCT = "MM1 HAT"
 USB_MANUFACTURER = "RoboticsMasters"
 
 # Non-Flash Edition
-INTERNAL_FLASH_FILESYSTEM = 1
-LONGINT_IMPL = NONE
+#INTERNAL_FLASH_FILESYSTEM = 1
+#LONGINT_IMPL = NONE
 
 # SPI-Flash Edition
 SPI_FLASH_FILESYSTEM = 1
@@ -17,11 +17,18 @@ LONGINT_IMPL = MPZ
 CHIP_VARIANT = SAMD21G18A
 CHIP_FAMILY = samd21
 
-#CFLAGS_INLINE_LIMIT = 55
+# Make room for frozen Libraries
+CIRCUITPY_DISPLAYIO = 0
+CIRCUITPY_FREQUENCYIO = 0
 
 # Include these Python libraries in firmware.
-#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_INA219
+#FROZEN_MPY_DIRS += $(TOP)/frozen/RoboticsMasters_CircuitPython_MPU9250
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 #FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Crickit
 #FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Motor
-#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 #FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_seesaw
+
+#Adding per @danh to reduce memory usage and get the latest changes in
+CFLAGS_INLINE_LIMIT = 55
