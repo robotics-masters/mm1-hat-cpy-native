10c0c84758414e3b4bbe26a5a5d94b9bbdcef49f
wallarug <wallarug@coliemore.com.au>
2019-03-07 23:27:49 +1100

    updated mm1_hat_nonflash to include BusDevice without needed it in the folder

--- experiments/usb-serial-debugger.py
+++ experiments/usb-serial-debugger.py
@@ -4,26 +4,18 @@ import board
 import busio
 import digitalio
  
- 
-led = digitalio.DigitalInOut(board.D13)
-led.direction = digitalio.Direction.OUTPUT
- 
-uart = busio.UART(board.TX, board.RX, baudrate=9600)
+uart = busio.UART(board.TX, board.RX, baudrate=115200)
 
 print("Running Debugger")
 
 while True:
-    data = uart.read(32)  # read up to 32 bytes
-    dataSend = local.read(32)
-    # print(data)  # this is a bytearray type
+    data = uart.read(32)  # read up to 64 bytes
+    #print(data)  # this is a bytearray type
 
     if data is not None:
-        led.value = True
- 
         # convert bytearray to string
         data_string = ''.join([chr(b) for b in data])
         print(data_string, end="")
- 
-        led.value = False
+
         
     
--- firmware/mm1_hat_nonflash/mpconfigboard.mk
+++ firmware/mm1_hat_nonflash/mpconfigboard.mk
@@ -1,7 +1,7 @@
 LD_FILE = boards/samd21x18-bootloader-crystalless.ld
 USB_VID = 0x0005
 USB_PID = 0x0006
-USB_PRODUCT = "MM1 HAT"
+USB_PRODUCT = "Robo HAT MM1"
 USB_MANUFACTURER = "Robotics Masters"
 
 # Non-Flash Edition
@@ -12,11 +12,18 @@ CIRCUITPY_SMALL_BUILD = 1
 CHIP_VARIANT = SAMD21G18A
 CHIP_FAMILY = samd21
 
-#CFLAGS_INLINE_LIMIT = 55
+# Make room for frozen Libraries
+CIRCUITPY_DISPLAYIO = 0
+CIRCUITPY_FREQUENCYIO = 0
 
 # Include these Python libraries in firmware.
-#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_INA219
+#FROZEN_MPY_DIRS += $(TOP)/frozen/RoboticsMasters_CircuitPython_MPU9250
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 #FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Crickit
 #FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Motor
-#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 #FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_seesaw
+
+#Adding per @danh to reduce memory usage and get the latest changes in
+CFLAGS_INLINE_LIMIT = 55
