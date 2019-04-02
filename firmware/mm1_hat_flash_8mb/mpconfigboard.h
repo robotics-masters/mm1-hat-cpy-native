de71dd94e6b302692a237708a90ea94426e7ca55
wallarug <wallarug@coliemore.com.au>
2019-04-02 21:16:44 +1100

    updated pin mapping

--- firmware/mm1_hat_flash_8mb/mpconfigboard.h
+++ firmware/mm1_hat_flash_8mb/mpconfigboard.h
@@ -1,19 +1,16 @@
 #define MICROPY_HW_BOARD_NAME "Robo HAT MM1"
 #define MICROPY_HW_MCU_NAME "samd21g18"
 
-#define MICROPY_HW_LED_STATUS   (&pin_PA12)
-
-//#define MICROPY_HW_NEOPIXEL (&pin_PA06)
-
+#define MICROPY_HW_LED_STATUS   (&pin_PA21)
 
 // Salae reads 12mhz which is the limit even though we set it to the safer 8mhz.
 #define SPI_FLASH_BAUDRATE  (8000000)
 
 // On-board flash
-#define SPI_FLASH_MOSI_PIN          &pin_PA08
+#define SPI_FLASH_MOSI_PIN          &pin_PA12
 #define SPI_FLASH_MISO_PIN          &pin_PA14
-#define SPI_FLASH_SCK_PIN           &pin_PA09
-#define SPI_FLASH_CS_PIN            &pin_PA13
+#define SPI_FLASH_SCK_PIN           &pin_PA13
+#define SPI_FLASH_CS_PIN            &pin_PA15
 
 // These are pins not to reset.
 #define MICROPY_PORT_A        ( 0 ) //PORT_PA06
@@ -33,9 +30,9 @@
 #define DEFAULT_I2C_BUS_SCL (&pin_PA23)
 #define DEFAULT_I2C_BUS_SDA (&pin_PA22)
 
-#define DEFAULT_SPI_BUS_SCK (&pin_PA05)
-#define DEFAULT_SPI_BUS_MOSI (&pin_PA07)
-#define DEFAULT_SPI_BUS_MISO (&pin_PA06)
+#define DEFAULT_SPI_BUS_SCK (&pin_PB11)
+#define DEFAULT_SPI_BUS_MOSI (&pin_PB10)
+#define DEFAULT_SPI_BUS_MISO (&pin_PB08)
 
 #define DEFAULT_UART_BUS_RX (&pin_PB23)
 #define DEFAULT_UART_BUS_TX (&pin_PB22)
