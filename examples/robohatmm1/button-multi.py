9fb7b828eaa736b70ae2cb7eac841f9356e985c6
wallarug <wallarug@coliemore.com.au>
2020-06-25 22:44:38 +1000

    Working for long and short button presses

--- examples/robohatmm1/button-multi.py
+++ examples/robohatmm1/button-multi.py
@@ -11,16 +11,21 @@ button.pull = digitalio.Pull.DOWN
 
 TIME_BETWEEN_PRESS = 0.5
 pressed = False
-first_press = time.monotonic()
+first_press = 0
 
 while True:
     if pressed == False:
         pressed = button.value
         first_press = time.monotonic()
 
-    if pressed and (first_press + TIME_BETWEEN_PRESS > time.monotonic()):
+    if button.value == False and pressed and (first_press + TIME_BETWEEN_PRESS < time.monotonic()):
+        print("single press")
+        pressed = False
+    elif button.value and pressed and (first_press + TIME_BETWEEN_PRESS < time.monotonic()):
+        print("long press")
+        pressed = False
+        while button.value:
+            continue
         
-
-    
-    #led.value = button.value
+    led.value = button.value
     time.sleep(0.1)
