3637715471ac28b2f6516d9ff9929f0cb0ebf86d
wallarug <wallarug@coliemore.com.au>
2020-09-26 23:00:44 +1000

    rewrite with donkeycar code

--- examples/code-differential-TCIII.py
+++ examples/code-differential-TCIII.py
@@ -1,9 +1,12 @@
 # Donkey Car Driver for Robotics Masters Robo HAT MM1
 #
 # Notes:
-#   This is to be run using CircuitPython 5.x
+#   This is to be run using CircuitPython 5.3
 #   Date: 15/05/2019
-#   Updated: 14/09/2020 (wallarug)
+#   Updated: 20/02/2020
+#   Updated: 8/03/2020 (sctse999)
+#   Updated: 11/05/2020 (wallarug)
+#
 #
 
 import time
@@ -32,39 +35,30 @@ def servo_duty_cycle(pulse_ms, frequency = 60):
     """
     Formula for working out the servo duty_cycle at 16 bit input
     """
-    if(pulse_ms > 2000 or pulse_ms < 1000):
-	pulse_ms = 1500  # if the pulse length is invalid, set middle position
     period_ms = 1.0 / frequency * 1000.0
     duty_cycle = int(pulse_ms / 1000 / (period_ms / 65535.0))
     return duty_cycle
 
-def servo_diff_duty_cycle(pulse_ms, frequency = 60):
-    """
-    Formula for working out the servo duty_cycle at 16 bit input
-    """
-    if(pulse_ms > 2000 or pulse_ms < 1000):
-	pulse_ms = 1500  # if the pulse length is invalid, set middle position
-    if(pulse < 1500):
-        pulse_ms = (1500 - pulse) / 500
-    if(pulse > 1500):
-        pulse_ms = (2000 - pulse) / 500
-        
-    period_ms = 1.0 / frequency * 1000.0
-    duty_cycle = int(pulse_ms / 1000 / (period_ms / 65535.0))
-    return duty_cycle
 
-def motor_duty_cycle(pulse, frequency = 500):
-	duty_cycle = 0
-	if(pulse > 2000 or pulse < 1000):
-		return duty_cycle # if the pulse length is invalid, 0% duty cycle to turn the motor off
-	if(pulse < 1500):
-		motor_direction.value = False
-		duty_cycle = (1500 - pulse) / 500
-	if(pulse > 1500):
-		motor_direction.value = True
-		duty_cycle = (2000 - pulse) / 500
-	# here duty_cycle is between 0 and 100
-	return int(duty_cycle * 65535.0)
+def state_changed(control):
+    """
+    Reads the RC channel and smooths value
+    """
+    control.channel.pause()
+    for i in range(0, len(control.channel)):
+        val = control.channel[i]
+        # prevent ranges outside of control space
+        if(val < 1000 or val > 2000):
+            continue
+        # set new value
+        control.value = (control.value + val) / 2
+
+    if DEBUG:
+        logger.debug("%f\t%s (%i): %i (%i)" % (time.monotonic(), control.name, len(
+            control.channel), control.value, servo_duty_cycle(control.value)))
+    control.channel.clear()
+    control.channel.resume()
+
 
 class Control:
     """
@@ -76,31 +70,7 @@ class Control:
         self.servo = servo
         self.channel = channel
         self.value = value
-        self.servo.duty_cycle = update_function(value)
-        self.update_function = update_function
-
-    def duty_cycle(self, value):
-	self.servo.duty_cycle = self.update_function(value)
-	return self.servo.duty_cycle
-	    
-    def state_changed(self):
-        """
-        Reads the RC channel and smooths value
-        """
-        self.channel.pause()
-        for i in range(0, len(self.channel)):
-            val = self.channel[i]
-            # prevent ranges outside of control space
-            if(val < 1000 or val > 2000):
-                continue
-            # set new value
-            self.value = val
-
-        if DEBUG:
-            logger.debug("%f\t%s (%i): %i (%i)" % (time.monotonic(), self.name, len(
-                self.channel), self.value, self.servo_duty_cycle(self.value)))
-        self.channel.clear()
-        #control.channel.resume()
+        self.servo.duty_cycle = servo_duty_cycle(value)
 
 
 # set up on-board LED
@@ -111,28 +81,17 @@ led.direction = Direction.OUTPUT
 # note UART(TX, RX, baudrate)
 uart = busio.UART(board.TX1, board.RX1, baudrate=115200, timeout=0.001)
 
-## set up servo/motor outputs
-## For differential you will be using two motors, no servos
-## Pins are:
-left_motor = PWMOut(board.SERVO3, duty_cycle = servo_duty_cycle(1500), frequency = 60)
-right_motor = PWMOut(board.SERVO4, duty_cycle = servo_duty_cycle(1500), frequency = 60)
+# set up servos
+steering_pwm = PWMOut(board.SERVO3, duty_cycle=2 ** 15, frequency=60)
+throttle_pwm = PWMOut(board.SERVO4, duty_cycle=2 ** 15, frequency=60)
 
 # set up RC channels.  NOTE: input channels are RCC3 & RCC4 (not RCC1 & RCC2)
-throttle_channel = PulseIn(board.RCC3, maxlen=64, idle_state=0)
 steering_channel = PulseIn(board.RCC4, maxlen=64, idle_state=0)
-
+throttle_channel = PulseIn(board.RCC3, maxlen=64, idle_state=0)
 
 # setup Control objects.  1500 pulse is off and center steering
-# ASSUMPTION - left motor is on steering, right motor on throttle (for code purposes)
-steering = Control("Steering", left_motor, steering_channel, 1500, motor_duty_cycle)
-throttle = Control("Throttle", right_motor, throttle_channel, 1500, motor_duty_cycle)
-
-## Set some other variables
-SPEED_FACTOR = 1
-SMOOTHING_INTERVAL_IN_S = 0.05
-DEBUG = True
-last_update = time.monotonic()
-
+steering = Control("Steering", steering_pwm, steering_channel, 1500)
+throttle = Control("Throttle", throttle_pwm, throttle_channel, 1500)
 
 # Hardware Notification: starting
 logger.info("preparing to start...")
@@ -161,14 +120,11 @@ def main():
         last_update = time.monotonic()
 
         # check for new RC values (channel will contain data)
-        changed = False
         if(len(throttle.channel) != 0):
-            throttle.state_changed()
-            changed = True
+            state_changed(throttle)
 
         if(len(steering.channel) != 0):
-            steering.state_changed()
-            changed = True
+            state_changed(steering)
 
         if DEBUG:
             logger.info("Get: steering=%i, throttle=%i" % (int(steering.value), int(throttle.value)))
@@ -187,7 +143,7 @@ def main():
             # if no data, break and continue with RC control
             if(byte == None):
                 break
-            #last_input = time.monotonic()
+            last_input = time.monotonic()
 
             if (DEBUG):
                 logger.debug("Read from UART: %s" % (byte))
@@ -208,28 +164,26 @@ def main():
             throttle_val = throttle.value
             try:
                 steering_val = int(datastr[:4])
-                throttle_val = int(datastr[-4:]) * SPEED_FACTOR
-                last_input = time.monotonic()
+                throttle_val = int(datastr[-4:])
             except ValueError:
                 None
 
             data = bytearray('')
             datastr = ''
+            last_input = time.monotonic()
             logger.info("Set: steering=%i, throttle=%i" % (steering_val, throttle_val))
 
         if(last_input + 10 < time.monotonic()):
             # set the servo for RC control
-            # TODO - add in diff code here
             left_val = throttle.value - (1500 - steering.value)
             right_val = throttle.value + (1500 - steering.value)
-            steering.duty_cycle(left_val)
-            throttle.duty_cycle(right_val)
+            
+            steering.servo.duty_cycle = servo_duty_cycle(left_val)
+            throttle.servo.duty_cycle = servo_duty_cycle(right_val)
         else:
             # set the servo for serial data (recieved)
-            left_val = throttle.value - (1500 - steering.value)
-            right_val = throttle.value + (1500 - steering.value)
-            steering.duty_cycle(left_val)
-            throttle.duty_cycle(right_val)
+            steering.servo.duty_cycle = servo_duty_cycle(steering_val)
+            throttle.servo.duty_cycle = servo_duty_cycle(throttle_val)
 
 
 # Run
